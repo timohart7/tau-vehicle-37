a6bced6f20bcec6987cddd0d337329a0604bb373
timo.hartikainen <timo.hartikainen@tuni.fi>
2019-11-20 20:52:23 +0200

    Markup fixed

--- koodi.py
+++ koodi.py
@@ -1,8 +1,12 @@
+import os 
+import numpy as np
+import matplotlib.pyplot as plt
 import sklearn
+import cv2
 import tensorflow as tf
-import tf.keras
+import tensorflow.keras
 
-class_names =sorted(os.listdir(r"C:\Work\sgndataset\train\"))
+class_names = sorted(os.listdir('data'))
 
 base_model = tf.keras.applications.mobilenet.MobileNet(input_shape = (224,224,3),include_top = False)
 
@@ -13,16 +17,16 @@ out_tensor = base_model.outputs[0]# Grab the output of base model
 out_tensor = tf.keras.layers.GlobalAveragePooling2D()(out_tensor)# Define the full model by the endpoints.
 model = tf.keras.models.Model(inputs  = [in_tensor],outputs = [out_tensor])# Compile the model for execution. Losses and optimizers
 # can be anything here, since we don’t train the model.
-model.compile(loss = "categorical_crossentropy", optimizer = ’sgd’)
+model.compile(loss = 'categorical_crossentropy', optimizer = 'sgd')
 
 # Find all image files in the data directory.
 
-X = []# Feature vectors will go here.
-y = []# Class ids will go here.
+X = [] # Feature vectors will go here.
+y = [] # Class ids will go here.
 
-for root, dirs, filesinos.walk(r"C:\Work\sgndataset\train\"):
+for root, dirs, files in os.walk('data'):
     for name in files:
-        if name.endswith(".jpg"):
+        if name.endswith('.jpg'):
             # Load the image:
             img = plt.imread(root + os.sep + name)
             # Resize it to the net input size:
